a5bde6aae855ef27f7cd743ba863a7c643349672
Trevor Alexander <talex@privatdemail.net>
2014-09-25 16:12:24 -1000

    Fixed import preview for MATLAB format.

--- prepack/gui/import_interface_mat.R
+++ prepack/gui/import_interface_mat.R
@@ -4,11 +4,11 @@ on_importmatfilechoosebutton_file_set <- function(action) {
   message("filebuttonactivated")
   previewselector <- builder$getObject("importmatfilechoosebutton")
   
-  previewfile <- previewselector$GetFilename()
+  rbci.env$previewfile <- previewselector$GetFilename()
   # get some options also
   
   # here we need to go to the backend to process the MAT
-  rbci.env$previewtext <<- rbci.preview.mat(previewfile)
+  rbci.env$previewtext <- rbci.preview.mat(rbci.env$previewfile)
 }
 
 on_importmatpreviewbutton_clicked <- function(action) {
@@ -20,5 +20,16 @@ on_importmatpreviewbutton_clicked <- function(action) {
   # update options based on data characteristics (available cols, etc.)
   
   # add cols to listview
+  collist <- builder$getObject("importoptslist")
+  coliter <- gtkListStoreAppend(collist)
+  # gtkListStoreSet(collist,coliter$iter, 0, "thing", -1)
   
+  gtkListStoreSetValue(collist,coliter$iter, 0, "thing")
+  
+#   sapply("thing",
+#          function(string) {
+#            ## Add a new row to the model
+#            iter <- collist$append(coliter)$iter
+#            collist$set(iter, 0, string, 1, i, 2,  FALSE)
+#          })
 }
