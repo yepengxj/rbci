6f3118b4577fc1f868959ca8bac4e1c3221b43c5
Trevor Alexander <talex@privatdemail.net>
2014-10-29 13:15:45 -1000

    Added base GUI for type 2 MATLAB files (epoched/split structure)

--- prepack/backend/init_backend.R
+++ prepack/backend/init_backend.R
@@ -1,10 +1,19 @@
 ###### Initialize backend processing #######
-rbci.env <- new.env()
+rbci.env <- new.env() # environment for storing GUI state stuff
 rbci.env$opts <- list()
 rbci.env$importlist <- list()
 rbci.env$filterlist <- list()
 rbci.env$transformlist <- list()
 
-library(data.table)
 library(tools)
 library(parallel)
+library(foreach)
+
+library(R.matlab) # for importing matfiles
+
+library(abind)
+library(reshape2)
+library(gdata) # for cleanups: keep()
+library(data.table)
+
+library(ggplot2)
--- prepack/gwidgets/import_interface.R
+++ prepack/gwidgets/import_interface.R
@@ -1,6 +1,5 @@
 source("./backend/previewers.R")
 source("./backend/importers.R")
-library(R.matlab)
 
 window.width <- 1000
 window.height <- 600
@@ -15,5 +14,6 @@ import_tabs <- gnotebook(tab.pos = 2,
                 container = import_win)
 
 source("./gwidgets/import_interface_matlab.R")
+source("./gwidgets/import_interface_matlab_type2.R")
 source("./gwidgets/import_interface_csv.R")
 source("./gwidgets/import_interface_bci2000.R")
