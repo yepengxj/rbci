c4ae63e1b278a5c8e8d6b13fbe4199570630f598
Trevor Alexander <talex@privatdemail.net>
2014-10-30 16:36:03 -1000

    partial test script for type2 import

---
+++ prepack/backend/type2import.R
@@ -0,0 +1,18 @@
+library(R.matlab)
+library(data.table)
+library(ggplot2)
+library(reshape2)
+
+init.struct <- readMat("./s1_AU_1_epoched.mat")
+
+init.eeg <- init.struct$eeg[[1]]
+init.tgt <- init.struct$eeg[[3]]
+
+eeg.table <- melt(init.eeg,
+                  varnames = c("Trial","Sample","Channel"),
+                  value.name = "Voltage")
+eeg.table <- as.data.table(eeg.table)
+
+eeg.table[,Target:=init.tgt]
+
+qplot(eeg.table[,Trial],eeg.table[,Target])
