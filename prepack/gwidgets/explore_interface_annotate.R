dfd9e607f7e44d7b6308702755f913d79019121b
Trevor Alexander <talex@privatdemail.net>
2014-11-12 13:57:10 -1000

    Data type inspector for annotator GUI.
    Minor formatting changes to annotator GUI.

--- prepack/gwidgets/explore_interface_annotate.R
+++ prepack/gwidgets/explore_interface_annotate.R
@@ -1,22 +1,40 @@
 ## only changes variable types for now (remember to document)
 
 annotate_mainframe <- gpanedgroup(horizontal = TRUE,
-                            expand = TRUE,
-                            fill = TRUE,
-                            container = annotate_tab)
+                                  expand = TRUE,
+                                  fill = TRUE,
+                                  container = annotate_tab)
 
 
 annotate_varlist_frame <- gframe(text = "Data Columns",
-                                horizontal = FALSE,
-                                container = annotate_mainframe,
-                                expand = TRUE,
-                                width = 300)
+                                 horizontal = FALSE,
+                                 container = annotate_mainframe,
+                                 expand = TRUE,
+                                 width = 300)
 # populate varlist
 annotate_varlist <- gradio(
   names(rbci.env$importlist[[svalue(explore_var_filesel, index=TRUE)]]),
   container = annotate_varlist_frame,
- # use.table = TRUE,
-  expand = FALSE)
+  # use.table = TRUE,
+  expand = FALSE,
+  handler = function(h, ...) {
+    
+    # update current datatype
+    svalue(annotate_curvartype) <-
+      paste("Current Data Type:", 
+            sapply(rbci.env$importlist[[svalue(explore_var_filesel)]],
+                   class)[svalue(annotate_varlist)])
+    
+    # update current tag
+    svalue(annotate_curtarget) <- 
+      paste("Current Target Column:",
+            rbci.env$tags[svalue(explore_var_filesel)]$targetcol)
+            
+    # update current tag
+    svalue(annotate_curvalue) <- 
+      paste("Current Value Column:",
+            rbci.env$tags[svalue(explore_var_filesel)]$valuecol)
+  })
 
 addSpring(annotate_varlist_frame)
 
@@ -38,29 +56,38 @@ annotate_optframe <- gframe(text = "Annotation Options",
 
 # variable type
 glabel("Data Type",
-       container = annotate_optframe)
-glabel("Current Data Type",
-       container = annotate_optframe)
+       container = annotate_optframe,
+       anchor = c(-1,1))
+# glabel("Current Data Type",
+#        container = annotate_optframe,
+#        anchor = c(-1,1))
 annotate_curvartype <- glabel("Current Data Type: ",
-                              container = annotate_optframe)
+                              container = annotate_optframe,
+                              anchor = c(-1,1))
 
 glabel("New Data Type",
-       container = annotate_optframe)
+       container = annotate_optframe,
+       anchor = c(-1,1))
 annotate_datatype_chooser <- 
   gdroplist(c("Numeric","Integer","Complex","Logical","Character"),
-            container = annotate_optframe)
+            container = annotate_optframe,
+            anchor = c(-1,1))
 
 
 # interest tag: target, value
 glabel("Column Tag",
-       container = annotate_optframe)
+       container = annotate_optframe,
+       anchor = c(-1,1))
 
 annotate_datatype_chooser <- 
   gdroplist(c("Target Column", "Value Column"),
-            container = annotate_optframe)
+            container = annotate_optframe,
+            anchor = c(-1,1))
 annotate_curtarget <- glabel("Current Target Column:",
-                             container = annotate_optframe)
+                             container = annotate_optframe,
+                             anchor = c(-1,1))
 annotate_curvalue <- glabel("Current Value Column:",
-                            container = annotate_optframe)
+                            container = annotate_optframe,
+                            anchor = c(-1,1))
 
 svalue(annotate_mainframe) <- 0.25
