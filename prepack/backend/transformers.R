94d219cca247af3ac19c0d9a4f3aaa32272235e5
Trevor Alexander <talex@privatdemail.net>
2014-11-28 18:36:44 -1000

    simplified formula input to linear PCA backend

--- prepack/backend/transformers.R
+++ prepack/backend/transformers.R
@@ -12,35 +12,52 @@ transform.pca <- function(targ.name = "Class",
                           pca.tol = sqrt(.Machine$double.eps)) {
 
     if (kernel.type == "Linear") {
-    
-        eeg.pca <- prcomp(as.formula(paste("~ .",
-                                           "-",targ.name,
-                                           "-",epoch.name,
-                                           "-",time.name)),
+        eeg.pca <- prcomp(reformulate(termlabels =
+                                          setdiff(colnames(eeg.table),
+                                                  c(targ.name,
+                                                    epoch.name,
+                                                    time.name))),
                           data = eeg.table,
                           scale = scale.data,
                           tol = pca.tol)
-
-        ## pull the pieces we need from each object into standard list
+        
+        ## pull the pieces we need from each object into standard list?
+        ## for now, since it's not very big, let's just keep what we get
+        return(eeg.pca)
     }
     else {
         ## nonlinear case
         switch(kernel.type,
                Gaussian = {
                                         # options: sigma
-                   eeg.pca <- kpca(eeg.table, kernel = "rbfdot",
+                   eeg.pca <- kpca(as.formula(paste("~ .",
+                                                    "-",targ.name,
+                                                    "-",epoch.name,
+                                                    "-",time.name)),
+                                   data = eeg.table,
+                                   kernel = "rbfdot",
                                    kpar = list(sigma = rbci.env$pcaopts$sigma),
                                    features = pc.count, th = pca.tol)
                },
                Laplace = {
                                         # options: sigma
-                   eeg.pca <- kpca(eeg.table, kernel = "laplacedot",
+                   eeg.pca <- kpca(as.formula(paste("~ .",
+                                                    "-",targ.name,
+                                                    "-",epoch.name,
+                                                    "-",time.name)),
+                                   data = eeg.table,
+                                   kernel = "laplacedot",
                                    kpar = list(sigma = rbci.env$pcaopts$sigma),
                                    features = pc.count, th = pca.tol)
                },
                Polynomial = {
                                         # options: degree, scale, offset
-                   eeg.pca <- kpca(eeg.table, kernel = "polydot",
+                   eeg.pca <- kpca(as.formula(paste("~ .",
+                                                    "-",targ.name,
+                                                    "-",epoch.name,
+                                                    "-",time.name)),
+                                   data = eeg.table,
+                                   kernel = "polydot",
                                    kpar = list(degree = rbci.env$pcaopts$degree,
                                                scale = rbci.env$pcaopts$scale,
                                                offset = rbci.env$pcaopts$offset),
@@ -48,14 +65,24 @@ transform.pca <- function(targ.name = "Class",
                },
                Hyperbolic = {
                                         # options: scale, offset
-                   eeg.pca <- kpca(eeg.table, kernel = "tanhdot",
+                   eeg.pca <- kpca(as.formula(paste("~ .",
+                                                    "-",targ.name,
+                                                    "-",epoch.name,
+                                                    "-",time.name)),
+                                   data = eeg.table,
+                                   kernel = "tanhdot",
                                    kpar = list(scale = rbci.env$pcaopts$scale,
                                                offset = rbci.env$pcaopts$offset),
                                    features = pc.count, th = pca.tol)
                },
                Bessel = {
                                         # options: sigma, order, degree
-                   eeg.pca <- kpca(eeg.table, kernel = "besseldot",
+                   eeg.pca <- kpca(as.formula(paste("~ .",
+                                                    "-",targ.name,
+                                                    "-",epoch.name,
+                                                    "-",time.name)),
+                                   data = eeg.table,
+                                   kernel = "besseldot",
                                    kpar = list(sigma = rbci.env$pcaopts$sigma,
                                                order = rbci.env$pcaopts$order,
                                                degree = rbci.env$pcaopts$degree),
@@ -63,14 +90,16 @@ transform.pca <- function(targ.name = "Class",
                },
                ANOVA = {
                                         # options: sigma, degree
-                   eeg.pca <- kpca(eeg.table, kernel = "laplacedot",
+                   eeg.pca <- kpca(as.formula(paste("~ .",
+                                                    "-",targ.name,
+                                                    "-",epoch.name,
+                                                    "-",time.name)),
+                                   data = eeg.table,
+                                   kernel = "laplacedot",
                                    kpar = list(sigma = rbci.env$pcaopts$sigma,
                                                degree = rbci.env$pcaopts$degree),
                                    features = pc.count, th = pca.tol)
                })
-
-        ## pull the pieces we need from the resulting object into standard list
+        
     }
-
-    ## return list object we created
 }
