03650e4c9b0110709b9443e4d3623aabe92daf9d
Trevor Alexander <talex@privatdemail.net>
2015-01-07 15:00:17 -1000

    converted filter to Hz, fixed plot

--- prepack/backend/plot_filter.R
+++ prepack/backend/plot_filter.R
@@ -4,7 +4,7 @@ plot.filter <- function(filter, sample.rate = 1) {
     filter.char <- freqz(filter, Fs = sample.rate)
     filter.char <- data.frame(freq = filter.char$f,
                               mag = 20 * log10( abs(filter.char$h ) ) )
-
+    
     ggplot(filter.char, aes(x = freq, y = mag)) + xlim(0,20) + ylim(-30,5) + 
         geom_line() + 
         ggtitle(bquote("Magnitude Response of Applied Filter")) +
