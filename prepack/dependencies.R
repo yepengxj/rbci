36a31efde0e95c08793f85d1ef3918437cf71223
Trevor Alexander <talex@privatdemail.net>
2015-01-14 14:21:03 -1000

    automated dependencies list

--- prepack/dependencies.R
+++ prepack/dependencies.R
@@ -1,22 +1,47 @@
 ### libraries required
 ## should be consolidated here like a package.json file in node
 
-library(tools)
-library(devtools)   # for installing github libs
-library(parallel)
-library(foreach)    # explicit parallel frontend
-library(doMC)       # local multicore backend
-library(R.matlab)   # for importing matfiles
-library(signal)     # signal processing lib
-library(abind)      # multivariate array concatenator
-library(R.utils)    
-library(reshape2)   # for dcast, melt
-library(gdata)      # for cleanups: keep()
-library(data.table) # hyperfast memory-efficient data struct
-library(ggplot2)    # general plotting engine
-library(ggbiplot)   # ggidevtools-dependent
-library(JADE)       # joint diagonalization
-library(psych)      # geometric, harmonic means
-library(cluster)    # clusplot, silhouette
-library(sda)        # sda classifier
-library(LiblineaR)  # linear svm classifier
+cran.dependencies = c(
+    "tools"     ,  
+    "devtools"  ,  # for installing github libs
+    "parallel"  ,  # TODO needed?
+    "foreach"   ,  # explicit parallel frontend
+    "doMC"      ,  # local multicore backend
+    "R.matlab"  ,  # for importing matfiles
+    "signal"    ,  # signal processing lib
+    "abind"     ,  # multivariate array concatenator
+    "R.utils"   ,  
+    "reshape2"  ,  # for dcast, melt
+    "gdata"     ,  # for cleanups: keep""
+    "data.table",  # hyperfast memory-efficient data struct
+    "ggplot2"   ,  # general plotting engine
+    "JADE"      ,  # joint diagonalization
+    "psych"     ,  # geometric, harmonic means
+    "cluster"   ,  # clusplot, silhouette
+    "sda"       ,  # sda classifier
+    "LiblineaR"    # linear svm classifier    
+    )
+
+github.dependencies = list(
+    ggbiplot = "vqv/ggbiplot"  # ggidevtools-dependent
+    )
+
+## install and load missing dependencies
+lapply(cran.dependencies, function(x) {
+    if (!(require(x, character.only = TRUE))) {
+        install.packages(x)
+    }
+    
+    library(x, character.only = TRUE)
+})
+
+lapply(github.dependencies, function(x) {
+    if (!(require(x, character.only = TRUE))) {
+        install_github(x)
+    }
+
+    package.basename <- eval.parent(quote(names(X)))[substitute(x)[[3]]]
+    ## print(package.basename)
+    library(package.basename,
+            character.only = TRUE)
+})
