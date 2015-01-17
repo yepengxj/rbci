bcc7cba8dee2fa271056440550275ff0cfa926f1
Trevor Alexander <talex@privatdemail.net>
2015-01-17 13:29:09 -1000

    added core detection to opts GUI logic

--- prepack/gwidgets/opts_interface.R
+++ prepack/gwidgets/opts_interface.R
@@ -1,7 +1,5 @@
-window.width <- 250
-window.height <- 400
-
-preview.rowlen <- 20
+window.width <- 450
+window.height <- 500
 
 opts_win <- gwindow("Parallelization Options",
                     width = window.width,
@@ -40,17 +38,23 @@ parallelbackend_type_menu <-
 
 backendopts_layout <- glayout(container = parallel_frame)
 backendopts_layout[1,1] <- "# of Cores"
-backendopts_layout[2,1] <- gslider(from = 1, to = detectCores(), by = 1,
-                                   handler = function(h,...){
-                                     # set number of cores
-                                     rbci.env$numcores <- svalue(h$obj)
-                                  })
 
-backendopts_layout[1,2] <- ""
-backendopts_layout[2,2] <- gspinbutton(from = 0, to = 1, by = 0.01)
+if (detectCores() < 2) { # single core case
+    backendopts_layout[2,1] <-
+        "Single core machine detected: multicore unavailable"
+} else { 
+    backendopts_layout[2,1] <- gslider(from = 1, to = detectCores(), by = 1,
+                                       handler = function(h,...){
+                                        # set number of cores
+                                           rbci.env$numcores <- svalue(h$obj)
+                                       })
+}
 
-backendopts_layout[3,1] <- ""
-backendopts_layout[4,1] <- gspinbutton(from = 0, to = 1, by = 0.01)
+# backendopts_layout[1,2] <- " "
+# backendopts_layout[2,2] <- gspinbutton(from = 0, to = 1, by = 0.01)
+# 
+# backendopts_layout[3,1] <- " "
+# backendopts_layout[4,1] <- gspinbutton(from = 0, to = 1, by = 0.01)
 
 enabled(backendopts_layout[1,1]) <- TRUE
 enabled(backendopts_layout[2,1]) <- TRUE
