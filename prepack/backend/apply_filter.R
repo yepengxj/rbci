03bcf974eb7fe209e8aa5bd77bb1f0c8bf9cfb6f
Trevor Alexander <talex@privatdemail.net>
2015-01-18 19:57:58 -1000

    fixed indent/delimiter typo in filter backend

--- prepack/backend/apply_filter.R
+++ prepack/backend/apply_filter.R
@@ -7,18 +7,20 @@ apply.filter <- function(signal.table, filt.groups, val.col, filter.obj) {
     setkeyv(table.filtered, filt.groups[[1]])
 
     if (getDoParRegistered()) { # parallel case
-        foreach(this.group =
-                    table.filtered[J(this.group),
-                                   unique(get(filt.groups[[1]]))]) %dopar% {
-                                       table.filtered[, c(val.col) :=
-                                                          filtfilt(filter.obj,
-                                                                   get(val.col)),
-                                                      by = c(filt.groups[[2]])]
-                                     } else {
-                                     table.filtered <-
-                                       table.filtered[, c(val.col) := filtfilt(filter.obj, get(val.col)),
-                                                      by = filt.groups]
-                                   }
-                                     return(table.filtered)
-    ### TODO examine reference dispatch in distributed context
+        foreach(
+            this.group =
+                table.filtered[J(this.group),
+                               unique(get(filt.groups[[1]]))]) %dopar% {
+                                   table.filtered[, c(val.col) :=
+                                                      filtfilt(filter.obj,
+                                                               get(val.col)),
+                                                  by = c(filt.groups[[2]])]
+                               }
+    } else {
+        table.filtered <-
+            table.filtered[, c(val.col) := filtfilt(filter.obj, get(val.col)),
+                           by = filt.groups]
+    }
+    return(table.filtered)
+### TODO examine reference dispatch in distributed context
 }
