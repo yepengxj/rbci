59d3818ea4ac4d4a4d1db4b353d03445601be509
Trevor Alexander <talex@privatdemail.net>
2015-01-25 00:45:15 -1000

    removed unneeded single export button from reporter GUI

--- prepack/gwidgets/report_interface_text.R
+++ prepack/gwidgets/report_interface_text.R
@@ -18,19 +18,6 @@ step_code_text <- gtext(container = step_code_frame, # step_code_frame,
                         font.attr = c(family="monospace"),
                         expand = TRUE)
 
-step_run_export <-
-    gbutton("Run This Step and Save",
-            container = step_code_frame,
-            handler = function(h,...){
-                this.step.code <-
-                    svalue(process_step_sel,"code")
-                
-                save(export.single.step(this.step.code),
-                     file = gfile(
-                         filter = list("RData" = list(patterns = c("*.RData"))),
-                         type = "save"))
-            })
-
 report_opts_frame <- gframe("Report Options",
                             horizontal = TRUE,
                             container = text_pane_group)
