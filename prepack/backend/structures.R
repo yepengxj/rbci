363011429e7f70123a1d2daba0be6bfaff40b956
Trevor Alexander <talex@privatdemail.net>
2015-01-25 00:01:13 -1000

    unneeded data structures removed

--- prepack/backend/structures.R
+++ prepack/backend/structures.R
@@ -4,18 +4,6 @@ rbci.env$opts <- list()
 ## list for datasets
 ### TODO rename 
 rbci.env$importlist <- list()
-## tags for annotation
-## hierarchy:
-## rbci.env
-##  -> tags
-##   -> data name
-##    -> targetcol
-##       valuecol
-rbci.env$tags <- list() 
-rbci.env$taglist <- list(targetcol = NULL,
-                         valuecol = NULL,
-                         epochcol = NULL)
-
 ### list for storing steps for later reporting
 ## hierarchy:
 ## step
@@ -24,22 +12,13 @@ rbci.env$taglist <- list(targetcol = NULL,
 ##  -> enabled boolean
 rbci.env$steplist <- list()
 ## example/test table:
-rbci.env$steplist <-
-    list(step1 =
-             list(summary = "transform.pca of set.1",
-                  enabled = TRUE,
-                  code = "transform.pca(set.1,\nsome.params"),
-         step2 =
-             list(summary = "downsample.dt of set.2",
-                  enabled = FALSE,
-                  code = "downsample.dt(set.2)")
-         )
-
-### TODO revisit these
-rbci.env$filterlist <- list()
-## list for computed transforms
-rbci.env$transformlist <- list()
-## list for transformed data
-rbci.env$transdatalist <- list()
-
-# gc() # ensure we have a clean slate
+# rbci.env$steplist <-
+#     list(step1 =
+#              list(summary = "transform.pca of set.1",
+#                   enabled = TRUE,
+#                   code = "transform.pca(set.1,\nsome.params"),
+#          step2 =
+#              list(summary = "downsample.dt of set.2",
+#                   enabled = FALSE,
+#                   code = "downsample.dt(set.2)")
+#          )
