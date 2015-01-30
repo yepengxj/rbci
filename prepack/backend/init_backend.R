b6191e66efd87d0aa0df6e66eacca27282ee55d4
Trevor Alexander <talex@privatdemail.net>
2015-01-30 10:58:45 -1000

    reverted bulk import in backend, contains loop

--- prepack/backend/init_backend.R
+++ prepack/backend/init_backend.R
@@ -2,18 +2,16 @@
 ### load libs
 source("./backend/dependencies.R")
 
-### load all backend files, including those added by user
-sourceDirectory("./backend")
-## ### load data structures
-## source("./backend/structures.R")
-## 
-## ### misc utility functions
-## source("./backend/misc.R")
-## ### importer backend
-## source("./backend/importers.R")
-## ### explorer backend
-## source("./backend/explorers.R")
-## ### transformer backend
-## source("./backend/transformers.R")
-## ### reporter backend
-## source("./backend/reporter.R")
+### load data structures
+source("./backend/structures.R")
+
+### misc utility functions
+source("./backend/misc.R")
+### importer backend
+source("./backend/importers.R")
+### explorer backend
+source("./backend/explorers.R")
+### transformer backend
+source("./backend/transformers.R")
+### reporter backend
+source("./backend/reporter.R")
