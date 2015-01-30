d16de9d89f1c8aff4af4e91dac76b4d5e4988769
Trevor Alexander <talex@privatdemail.net>
2015-01-30 10:46:48 -1000

    init backend now bulk imports

--- prepack/backend/init_backend.R
+++ prepack/backend/init_backend.R
@@ -2,23 +2,18 @@
 ### load libs
 source("./backend/dependencies.R")
 
-### load data structures
-source("./backend/structures.R")
-
-### misc utility functions
-source("./backend/misc.R")
-### importer backend
-source("./backend/importers.R")
-### explorer backend
-source("./backend/explorers.R")
-### filter backend
-source("./backend/simple_filter.R")
-source("./backend/plot_filter.R")
-source("./backend/apply_filter.R")
-source("./backend/downsample_dt.R")
-### transformer backend
-source("./backend/transformers.R")
-### classifier backend
-source("./backend/classifiers.R")
-### reporter backend
-source("./backend/reporter.R")
+### load all backend files, including those added by user
+sourceDirectory("./backend")
+## ### load data structures
+## source("./backend/structures.R")
+## 
+## ### misc utility functions
+## source("./backend/misc.R")
+## ### importer backend
+## source("./backend/importers.R")
+## ### explorer backend
+## source("./backend/explorers.R")
+## ### transformer backend
+## source("./backend/transformers.R")
+## ### reporter backend
+## source("./backend/reporter.R")
